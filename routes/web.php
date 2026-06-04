58a106d3584bf65b802c5dab0a01d47eec18b23d
Boom-desgin <reachselwyn1566@gmail.com>
2026-06-04 10:40:00 +0800

    Restore Product routes for Branch 1: feature-create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Product Routes - Branch 1: Create/Add functionality
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
